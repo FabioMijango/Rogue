e3ff429eddd8b9d2858538e27be8f3d8d4a4c649
FabioMijango <00156022@uca.edu.sv>
2025-12-02 21:50:57 -0600

    Add Random number generation utility

---
+++ src/core/Random.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <cstdint>
+#include <ctime>
+
+namespace rnd {
+    inline static uint64_t x = time(nullptr);
+
+    inline uint64_t Get64() {
+        uint64_t z = (x += 0x9e3779b97f4a7c15);
+        z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+        z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+        return z ^ (z >> 31);
+    }
+
+    inline bool GetBit() {
+        return Get64() & 1;
+    }
+
+    inline uint32_t Get32() {
+        return static_cast<uint32_t>(Get64());
+    }
+
+    inline float GetFP32() {
+        union {
+            uint32_t i;
+            float f;
+        } u{};
+        u.i = (Get32() >> 9) | 0x3F800000;
+        return u.f - 1.0f;
+    }
+
+    inline double GetFP64() {
+        union {
+            uint64_t i;
+            double d;
+        } u{};
+        u.i = (Get64() >> 12) | 0x3FF0000000000000;
+        return u.d - 1.0;
+    }
+}
