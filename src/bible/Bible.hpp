3703073f27fc643857de70d78b84c12770bb9ff7
FabioMijango <00156022@uca.edu.sv>
2025-12-08 18:35:47 -0600

    Add tile index arrays.

--- src/bible/Bible.hpp
+++ src/bible/Bible.hpp
@@ -13,4 +13,14 @@ namespace bb {
         {"tiles", "./res/tiles.png", "./res/tiles.txt"},
     };
 
+    inline const uint16_t wallTopTiles[] = { 0, 2, 4, 6, 8, 10};
+    inline const uint16_t wallSideTiles[] = { 1, 3, 5, 7, 9, 11};
+    inline const uint16_t floorTiles[] = { 12, 19, 47, 54, 58, 66};
+    inline const uint16_t floorDecorationTiles[] =
+        { 16, 17, 18,
+        23, 24, 25,
+        30, 31, 32,
+        37, 38, 39,
+        44, 45, 46,
+        51, 52, 53};
 }
