df6b946d161eb21e03e2c8f12f63f898d3fc5c35
FabioMijango <00156022@uca.edu.sv>
2025-12-08 18:37:47 -0600

    Add DungeonTheme and DungeonThemeBuilder for dungeon theme generation

---
+++ src/bible/Theme.cpp
@@ -0,0 +1,41 @@
+#include "Theme.hpp"
+
+DungeonTheme DungeonThemeBuilder::generateDungeonTheme() {
+    setVariables();
+    generateTheme();
+    generateDungeon();
+    return dungeonTheme;
+}
+
+void DungeonThemeBuilder::setVariables() {
+    size = std::size(bb::wallTopTiles);
+
+    wallNumber = rnd::Get64() % size;
+    floorNumber = rnd::Get64() % size;
+
+    floorDecorationNumber = rnd::Get64() % size;
+    floorDecorationNumber = floorDecorationNumber < 2 ? 3 : floorDecorationNumber;
+}
+
+void DungeonThemeBuilder::generateTheme() {
+    wallSet = {
+        bb::wallTopTiles[wallNumber],
+        bb::wallSideTiles[wallNumber]
+    };
+
+    std::set<uint16_t> decorationsSet;
+    while (decorationsSet.size() <= floorDecorationNumber) {
+        decorationsSet.insert(bb::floorDecorationTiles[rnd::Get64() % size]);
+    }
+    floorSet = {
+        bb::floorTiles[floorNumber],
+        decorationsSet
+    };
+}
+
+void DungeonThemeBuilder::generateDungeon() {
+    dungeonTheme = DungeonTheme{
+        wallSet,
+        floorSet
+    };
+}
---
+++ src/bible/Theme.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <cstdint>
+#include <set>
+
+#include "../bible/Bible.hpp"
+#include "../core/Random.hpp"
+
+struct WallSet {
+    uint16_t topId;
+    uint16_t sideId;
+};
+struct FloorSet {
+    uint16_t baseId;
+    std::set<uint16_t> decorations;
+};
+
+struct DungeonTheme {
+    WallSet wall;
+    FloorSet floor;
+};
+
+class DungeonThemeBuilder {
+public:
+    DungeonThemeBuilder() = default;
+    DungeonTheme generateDungeonTheme();
+private:
+    uint16_t size = 0;
+    uint16_t wallNumber = 0;
+    uint16_t floorNumber = 0;
+    uint16_t floorDecorationNumber = 0;
+
+    WallSet wallSet;
+    FloorSet floorSet;
+
+    DungeonTheme dungeonTheme;
+private:
+    void setVariables();
+    void generateTheme();
+    void generateDungeon();
+};
