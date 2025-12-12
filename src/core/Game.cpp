8ba0a6d17ee0a33cab2b810646a67a26ef4b7c3a
FabioMijango <00156022@uca.edu.sv>
2025-12-11 22:16:06 -0600

    Implement screen management in Game class with ScreenManager and StartScreen

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -4,40 +4,36 @@ Game::Game() {
     window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Rogue");
     window.setFramerateLimit(60);
 
+    const sf::View view({0.f, 0.f}, sf::Vector2f(window.getSize()));
+    window.setView(view);
+
+    screenManager.init();
+    StartScreen startScreen;
+    startScreen.onEnter(resourceManager, window);
+    screenManager.changeScreen(std::make_unique<StartScreen>(startScreen));
+
     delta = clock.restart().asSeconds();
 }
 
+void Game::handleInput() {
+    screenManager.handleInput();
+}
+
 void Game::update() {
-    return;
+    delta = clock.restart().asSeconds();
+    screenManager.update(delta);
 }
 
 void Game::draw() {
     window.clear();
-
+    screenManager.render();
     window.display();
 }
 
-
 void Game::run() {
-
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-            if (event->is<sf::Event::KeyPressed>()) {
-                if (event->getIf<sf::Event::KeyPressed>()->code == sf::Keyboard::Key::Escape)
-                    window.close();
-            }
-        }
-
-        delta = clock.restart().asSeconds();
-
+    while (window.isOpen()) {
+        handleInput();
         update();
-
         draw();
     }
 }
--- src/core/Game.hpp
+++ src/core/Game.hpp
@@ -2,6 +2,8 @@
 #include <SFML/Graphics.hpp>
 
 #include "ResourceManager.hpp"
+#include "../game/screenManager/ScreenManager.hpp"
+#include "../game/screenManager/StartScreen.hpp"
 
 class Game {
 public:
@@ -10,9 +12,11 @@ public:
 private:
     sf::RenderWindow window;
     ResourceManager resourceManager;
+    ScreenManager screenManager;
     sf::Clock clock;
     double delta;
 private:
+    void handleInput();
     void update();
     void draw();
 };
