90e29c36b9e6b9ff37ce4f66f9e1a193bf8445f1
FabioMijango <00156022@uca.edu.sv>
2025-12-17 16:33:00 -0600

    Add ScreenId enum and pending screen retrieval to Screen class

--- src/game/screens/Screen.hpp
+++ src/game/screens/Screen.hpp
@@ -4,6 +4,12 @@
 #include "../GameContext.hpp"
 #include "../../core/ResourceManager.hpp"
 
+enum class ScreenId {
+    None,
+    Start,
+    Game,
+};
+
 class Screen {
 public:
     virtual ~Screen() = default;
@@ -13,7 +19,11 @@ public:
     virtual void onEnter(GameContext context) = 0;
     virtual void onExit() = 0;
 
+    [[nodiscard]] ScreenId getPendingScreen() const {
+        return screenId;
+    }
 protected:
+    ScreenId screenId = ScreenId::None;
     sf::RenderWindow *window = nullptr;
     std::vector<std::shared_ptr<sf::Drawable>> drawables;
 };
