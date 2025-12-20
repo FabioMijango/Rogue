dde0d60598b03ac8392858533d9a4693cd5da77c
FabioMijango <00156022@uca.edu.sv>
2025-12-20 13:39:48 -0600

    Refactor Player input handling to use velocity

--- src/game/objects/entities/Player.cpp
+++ src/game/objects/entities/Player.cpp
@@ -1,30 +1,33 @@
 #include "Player.hpp"
 
-#include <utility>
-
 Player::Player(sf::Sprite sprite): Entity(sprite) {
     this->position = {0.0f, 0.0f};
 }
 
 void Player::handleInput(const std::optional<sf::Event> &event) {
-    if (const auto *keyPressed = event->getIf<sf::Event::KeyPressed>()) {
-        if (keyPressed->scancode == sf::Keyboard::Scan::W) {
-            position.y += -100.0f;
-        } else if (keyPressed->scancode == sf::Keyboard::Scan::S) {
-            position.y += 100.0f;
+    currentVelocity = {0.0f, 0.0f};
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::W)) {
+            currentVelocity.y += -velocity;
         }
-        if (keyPressed->scancode == sf::Keyboard::Scan::A) {
-            position.x += -100.0f;
-        } else if (keyPressed->scancode == sf::Keyboard::Scan::D) {
-            position.x += 100.0f;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::S)) {
+            currentVelocity.y += velocity;
         }
-    }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::A)) {
+            currentVelocity.x += -velocity;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::D)) {
+            currentVelocity.x += velocity;
+        }
 }
 
 void Player::update(double dt) {
+    position += currentVelocity * static_cast<float>(dt);
     sprite.setPosition(position);
 }
 
 void Player::draw(sf::RenderWindow &window) const {
     window.draw(sprite);
+    sf::View v = window.getView();
+    v.setCenter(position);
+    window.setView(v);
 }
