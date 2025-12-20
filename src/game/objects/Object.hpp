204f2c0a8dfd47f8f9237418b7acc79096ed59e6
FabioMijango <00156022@uca.edu.sv>
2025-12-20 13:39:33 -0600

    Add velocity management to Entity and refactor sprite handling in Object

--- src/game/objects/Object.hpp
+++ src/game/objects/Object.hpp
@@ -1,14 +1,19 @@
 #pragma once
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Sprite.hpp>
+#include <utility>
 
 class Object {
 public:
     sf::Vector2f position;
-    std::unique_ptr<sf::Sprite> sprite;
+    sf::Sprite sprite;
 public:
+    Object(sf::Sprite sprite);
     virtual ~Object() = default;
     virtual void update(double dt) = 0;
     virtual void draw(sf::RenderWindow &window) const = 0;
 
 };
+
+inline Object::Object(sf::Sprite sprite): sprite(std::move(sprite)) {
+}
--- src/game/objects/entities/Entity.hpp
+++ src/game/objects/entities/Entity.hpp
@@ -6,10 +6,16 @@ class Entity : public Object {
 protected:
     uint16_t maxHealth = 1;
     uint16_t health = maxHealth;
+    float velocity = 50.f;
+    sf::Vector2f currentVelocity;
     std::unique_ptr<State> currentState;
     std::unique_ptr<State> previousState;
 
 public:
+    explicit Entity(const sf::Sprite &sprite);
     void update(double dt) override = 0;
     void draw(sf::RenderWindow &window) const override = 0;
 };
+
+inline Entity::Entity(const sf::Sprite &sprite): Object(sprite) {
+}
