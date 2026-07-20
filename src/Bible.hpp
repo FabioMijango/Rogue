9e24dc72ddd09610f08183df5bb0dd4519d900b9
FabioMijango <00156022@uca.edu.sv>
2026-07-20 12:07:41 -0600

    Update: Add Constants

--- src/Bible.hpp
+++ src/Bible.hpp
@@ -11,7 +11,10 @@ namespace bb {
 
     constexpr float ASSETS_TILE_SIZE = 32.0f;
 
-    constexpr float ROOM_SIZE = 100.0f;
+    constexpr int NUMBER_OF_ROOMS = 5;
+    constexpr float ROOM_SIZE = 100.0f; // TODO: Change to a bigger value (Like 400-600), actual just for debugging
     constexpr uint32_t TILES_PER_ROOM = 10;
     constexpr float TILE_SIZE = ROOM_SIZE / TILES_PER_ROOM;
+
+    constexpr float PROB_TO_BACK_TO_START = 0.4f;
 }
