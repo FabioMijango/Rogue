3188bf39bf6e57fd7f2b91c47362258dced70732
FabioMijango <00156022@uca.edu.sv>
2026-07-21 20:20:58 -0600

    Add: constants for game usage in Bible.hpp

--- src/Bible.hpp
+++ src/Bible.hpp
@@ -9,6 +9,9 @@ namespace bb {
     const Resource rogueRes = {"rogues", "./res/images/rogues.png"};
     const Resource tileRes = {"tiles", "./res/images/tiles.png"};
 
+    constexpr int WIDTH = 1920;
+    constexpr int HEIGHT = 1080;
+
     constexpr float ASSETS_TILE_SIZE = 32.0f;
 
     constexpr int NUMBER_OF_ROOMS = 5;
@@ -17,4 +20,9 @@ namespace bb {
     constexpr float TILE_SIZE = ROOM_SIZE / TILES_PER_ROOM;
 
     constexpr float PROB_TO_BACK_TO_START = 0.4f;
+
+    constexpr std::string ANIMID_TOP_WALL = "topWall";
+    constexpr std::string ANIMID_SIDE_WALL = "sideWall";
+
+    constexpr std::string ANIMID_FLOOR = "floor";
 }
