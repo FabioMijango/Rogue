e810df6cf4607971676f94aee820265546c1c3a2
fabiomijango <fabiomijango@gmail.com>
2026-08-07 18:45:14 -0600

    Update: build comand

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
-cmake -B build
+
+if [[ $1 = "release" ]]; then
+    cmake -B build -DCMAKE_BUILD_TYPE=Release
+    echo "Building in Release mode"
+else
+    cmake -B build -DCMAKE_BUILD_TYPE=Debug
+    echo "Building in Debug mode"
+fi
 cmake --build build
